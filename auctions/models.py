b97e814a4bd2556ccdd5d923ca4e09adb710121e
Raselli <82221324+Raselli@users.noreply.github.com>
2022-04-13 21:02:22 +0200

    4/13/22 PM EoD

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,7 @@
 from turtle import title
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from views import listing
 
 class User(AbstractUser):
     pass
@@ -9,12 +10,13 @@ class Listing():
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=512)
     starting_bid = models.DecimalField(max_length=12)
+    current_bid = models.DecimalField(max_length=12)
     img_url = models.URLField()
     
     def __str__(self):
         return f"Title: {self.title},\n description: {self.description}, \n Starting bid: {self.starting_bid}, \n Image: {self.img_url}" 
 
-class Bid():
+class Bid(listing):
     pass
 
 class Comment():
--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
 from .models import User, Listing
 
@@ -14,6 +15,56 @@ def index(request):
         "listings": Listing.objects.all()
     })
 
+class CreateListing(forms.Form):
+    title = forms.CharField(label="Title of listing item", max_length=60)
+    description = forms.CharField(widget=forms.Textarea, label="Description of listing item")
+    starting_bid = forms.DecimalField(max_digits=8, decimal_places=2)
+    img_url = forms.URLField(max_length=200)
+
+def create_listing(request):
+    #TO DO
+    if request.method == "POST":
+        form = CreateListing(request.POST)
+        
+        if form.is_valid():
+            title = form.cleaned_data("title")
+            description = form.cleaned_data("description")
+            starting_bid = form.cleaned_data("starting_bid")
+            img_url = form.cleaned_data("img_url")
+            
+    # method == GET
+    else:
+        return render(request, "auctions/create_listing.html", {
+            "form": CreateListing()
+        }) 
+
+def listing():
+    #TO DO
+    pass
+
+def watchlist():
+    #TO DO
+    pass
+
+def categories():
+    #TO DO
+    pass
+
+def bidding(request):
+    # add lock to prevent bidding collision
+    bid = request.Post("bid")
+    if Listing.current_bid == Listing.starting_bid:
+        if bid < Listing.starting_bid:
+            message = "Your bid must be equal to or higher than starting bid."
+            return message
+        else:
+            pass
+    elif Listing.current_bid > Listing.starting_bid:
+        if Listing.current_bid < bid:
+            pass
+        else:
+           message = "Your bid must be higher than the currently highest bid."
+           return message
 
 def login_view(request):
     if request.method == "POST":
